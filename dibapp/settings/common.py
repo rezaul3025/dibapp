bd4f0e8a831ace5a8d6f442a61a555b79b866dbd
Rezaul Karim <rezaul.km105@gmail.com>
2022-01-26 23:34:34 +0100

    init commit3

--- dibapp/settings/common.py
+++ dibapp/settings/common.py
@@ -115,6 +115,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
--- userapp/serializers/EventSerializer.py
+++ userapp/serializers/EventSerializer.py
@@ -2,7 +2,11 @@ from rest_framework import serializers
 from userapp.models.event import Event
 
 class EventSerializer(serializers.ModelSerializer):
+    type_name = serializers.CharField(source='type.type', read_only=True)
+
     class Meta:
         model = Event
-        fields = '__all__'
+        #fields = '__all__'
+        fields = ('internal_id', 'title', 'description', 'date', 'type_name','users')
+
 
